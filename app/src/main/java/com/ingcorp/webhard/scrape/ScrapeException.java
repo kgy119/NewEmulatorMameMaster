236f9476c4e99d92cf38b77be4d10fb8375c729d
gunyoungkim <hoyaf798@gmail.com>
2025-07-01 15:37:54 +0900

    change package name

---
+++ app/src/main/java/com/ingcorp/webhard/scrape/ScrapeException.java
@@ -0,0 +1,7 @@
+package com.ingcorp.webhard.scrape;
+
+public class ScrapeException extends Exception {
+	public ScrapeException(String s) {
+		super(s);
+	}
+}
--- app/src/main/java/com/seleuco/mame4droid/scrape/ScrapeException.java
+++
@@ -1,7 +0,0 @@
-package com.seleuco.mame4droid.scrape;
-
-public class ScrapeException extends Exception {
-	public ScrapeException(String s) {
-		super(s);
-	}
-}

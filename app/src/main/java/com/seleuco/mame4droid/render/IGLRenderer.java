e18918f08bd474fc206c7d584272936bc25a91e2
gunyoungkim <hoyaf798@gmail.com>
2025-07-01 14:18:09 +0900

    Initial commit

---
+++ app/src/main/java/com/seleuco/mame4droid/render/IGLRenderer.java
@@ -0,0 +1,11 @@
+package com.seleuco.mame4droid.render;
+
+import com.seleuco.mame4droid.MAME4droid;
+
+public interface IGLRenderer {
+
+	public void setMAME4droid(MAME4droid mm);
+
+	public void changedEmulatedSize();
+
+}
